059edff99ade1028c4501156e034be51aeee4c36
Alex <sasakurilenko715@gmail.com>
2024-03-30 19:26:27 +0300

    Refactoring keyboards

--- keyboards/reply_keyboards.py
+++ keyboards/reply_keyboards.py
@@ -1,17 +1,29 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import KeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-
-start_keyboard =ReplyKeyboardMarkup(
-    keyboard=[
-        [
-            KeyboardButton(text="Ближайшие турниры"),
-            KeyboardButton(text="Ближайшие матчи"),
-        ],
-        [
-            KeyboardButton(text="Отслеживаемые команды"),
-            KeyboardButton(text="Результаты последних матчей")
-        ]
-    ],
-    resize_keyboard=True,
-    input_field_placeholder='Что вас интересует?'
+start_keyboard = ReplyKeyboardBuilder()
+start_keyboard.add(
+    KeyboardButton(text="Турниры"),
+    KeyboardButton(text="Ближайшие матчи"),
+    KeyboardButton(text="Отслеживаемые команды"),
+    KeyboardButton(text="Результаты последних матчей")
 )
+start_keyboard.adjust(2, 2)
+
+
+follow_teams_keyboard = ReplyKeyboardBuilder()
+follow_teams_keyboard.add(
+    KeyboardButton(text='Добавить команду'),
+    KeyboardButton(text='Удалить команду'),
+    KeyboardButton(text='Назад')
+)
+follow_teams_keyboard.adjust(2, 1)
+
+
+tournaments_keyboard = ReplyKeyboardBuilder()
+tournaments_keyboard.add(
+    KeyboardButton(text='Прошедшие'),
+    KeyboardButton(text='Предстоящие'),
+    KeyboardButton(text='Назад')
+)
+tournaments_keyboard.adjust(2,)
